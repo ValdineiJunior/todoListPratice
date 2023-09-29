85b8017f4471cb3cea52ccc904901549010fdca0
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-09-29 09:33:34 -0300

    alterando splice pra filter na função remove

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,27 +3,25 @@ import { TodoList } from "../components/TodoList";
 import { useState } from "react";
 
 export function Home() {
-    const [todos, setTodos] = useState(["Fazer café", "Estudar", "Passear com a Luna"])
+    const [todos, setTodos] = useState([
+        "Fazer café",
+        "Estudar",
+        "Passear com a Luna",
+    ]);
     function add(text: string) {
-        if (text.length > 0) {
-            setTodos(todos => {
-    
-                return [...todos, text]
-            })
+        if (text.trim().length > 0) {
+            setTodos((todos) => {
+                return [...todos, text];
+            });
         }
     }
     function remove(index: number) {
-
-        setTodos(todos => {
-            const todoRemove = [...todos]
-            todoRemove.splice(index, 1)
-            return [...todoRemove]
-        })
+        setTodos((todos) => todos.filter((_, i) => i !== index));
     }
     return (
         <main>
             <AddTodo add={add} />
             <TodoList todos={todos} remove={remove} />
         </main>
-    )
+    );
 }
